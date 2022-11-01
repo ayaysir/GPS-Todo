da412143300fa8a779e26044fc9ab7718cb62b67
ayaysir <yoonbumtae@gmail.com>
2022-11-02 00:47:05 +0900

    TodoUpdateVC: Add and enable submission of end locations

--- GPS-Todo/ViewController/TodoList/TodoUpdateTableViewController.swift
+++ GPS-Todo/ViewController/TodoList/TodoUpdateTableViewController.swift
@@ -13,10 +13,15 @@ import BGSMM_DevKit
 
 class TodoUpdateTableViewController: UITableViewController {
     
+    private let SECTION_END_COORDS = 3
+    
     @IBOutlet weak var txfTitle: UITextField!
     @IBOutlet weak var txvContent: UITextView!
     @IBOutlet weak var mapViewStart: MKMapView!
     @IBOutlet weak var barBtnSubmit: UIBarButtonItem!
+    @IBOutlet weak var stepperEndLocationCount: UIStepper!
+    @IBOutlet weak var lblEndLocationCount: UILabel!
+    @IBOutlet weak var mapViewEnd: MKMapView!
     
     private let locationManager = GPSLocationManager()
     private var lastLocation: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 0, longitude: 0)
@@ -25,6 +30,9 @@ class TodoUpdateTableViewController: UITableViewController {
     // ====== View Model ========= //
     var todoTitle = BehaviorRelay<String>(value: "")
     var content = BehaviorRelay<String>(value: "")
+    var endLocationCount = BehaviorRelay<Double>(value: 1.0)
+    var endLocations = BehaviorRelay<[CLLocationCoordinate2D]>(value: [])
+    
     var startCoord = BehaviorRelay<CLLocationCoordinate2D>(value: CLLocationCoordinate2D())
     
     var isValid: Observable<Bool> {
@@ -36,14 +44,39 @@ class TodoUpdateTableViewController: UITableViewController {
         }
     }
     
+    @objc func longPressMapEndLocation(gestureRecognizer: UITapGestureRecognizer) {
+        guard mapViewEnd.annotations.count < 5 else {
+            return
+        }
+        
+        if gestureRecognizer.state == .began {
+            let location = gestureRecognizer.location(in: mapViewEnd)
+            let coordinate = mapViewEnd.convert(location, toCoordinateFrom: mapViewEnd)
+                
+            // Add annotation:
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            mapViewEnd.addAnnotation(annotation)
+            annotation.title = "Place \(mapViewEnd.annotations.count)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.register(EndLocationCell.self, forCellReuseIdentifier: "TodoUpdate_MapView_EndLoc")
+        
+        // mapView start
         mapViewStart.delegate = self
         mapViewStart.setZoomByDelta(delta: pow(2, -13), animated: true)
         mapViewStart.isScrollEnabled = false
         centerAnnotation.title = "Current Location"
         mapViewStart.addAnnotation(centerAnnotation)
+        
+        // mapView End
+        mapViewEnd.delegate = self
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(longPressMapEndLocation))
+        mapViewEnd.addGestureRecognizer(longPressGesture)
         
         switch locationManager.authStatus {
         case .notDetermined, .authorizedAlways, .authorizedWhenInUse:
@@ -56,6 +89,15 @@ class TodoUpdateTableViewController: UITableViewController {
             break
         }
         
+        _ = endLocationCount.asObservable().subscribe(onNext: { [unowned self] endLocationCount in
+            lblEndLocationCount.text = "\(Int(endLocationCount))"
+            
+            let indexSection = IndexSet(SECTION_END_COORDS...SECTION_END_COORDS)
+            tableView.reloadSections(indexSection, with: .none)
+        })
+        
+        // _ = stepperEndLocationCount.rx.value.bind(to: endLocationCount)
+        
         _ = txfTitle.rx.text.map({ $0 ?? ""}).bind(to: todoTitle)
         _ = txvContent.rx.text.map({ $0 ?? ""}).bind(to: content)
         
@@ -63,30 +105,60 @@ class TodoUpdateTableViewController: UITableViewController {
             barBtnSubmit.isEnabled = isValid
         })
         
-        // barBtn Clicked
+        // Submit to Firebase - barBtn Clicked
         _ = barBtnSubmit.rx.tap.subscribe(onNext: { [unowned self] in
             print(todoTitle.value, content.value, startCoord.value)
             
+            let endCoords = mapViewEnd.annotations.map { "\($0.coordinate)" }
             let todo = Todo(title: todoTitle.value,
                             content: content.value,
                             startCoord: "\(startCoord.value)",
-                            endCoords: ["1", "2", "3"])
+                            endCoords: endCoords)
             print("To Update Todo:", todo)
             
             FirestoreTodo.shared.addPost(todoRequest: todo) { documenID in
                 SimpleAlert.present(message: "Success", title: "Add Todo") { _ in
                     self.navigationController?.popViewController(animated: true)
                 }
-                
             }
         })
+        
+        //
     }
+}
 
+extension TodoUpdateTableViewController {
+    
     // MARK: - Table view data source
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == SECTION_END_COORDS {
+            return Int(2 + mapViewEnd.annotations.count)
+        }
+        
+        return super.tableView(tableView, numberOfRowsInSection: section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == SECTION_END_COORDS && indexPath.row >= 2 {
+            let coordText = "\(mapViewEnd.annotations[indexPath.row - 2].coordinate)"
+            let cell = super.tableView(tableView, cellForRowAt: indexPath)
+            if let label = cell.contentView.subviews[0] as? UILabel {
+                label.text = coordText
+            }
+        }
+        
+        return super.tableView(tableView, cellForRowAt: indexPath)
+    }
 }
 
 extension TodoUpdateTableViewController: MKMapViewDelegate {
+    
+    // MARK: - MapView Delegate
     
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
         print(#function)
@@ -97,11 +169,43 @@ extension TodoUpdateTableViewController: MKMapViewDelegate {
     }
     
     func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
+        
+        switch mapView {
+        case mapViewStart:
+            let zoomWidth = mapView.visibleMapRect.size.width
+            let zoomFactor = Int(log2(zoomWidth)) - 9
+            startCoord.accept(mapView.centerCoordinate)
+        case mapViewEnd:
+            break
+        default:
+            break
+        }
+        
+        // print(#function)
+        // print("...REGION DID CHANGE: ZOOM FACTOR \(zoomFactor)")
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, didChange newState: MKAnnotationView.DragState, fromOldState oldState: MKAnnotationView.DragState) {
         print(#function)
-        let zoomWidth = mapView.visibleMapRect.size.width
-        let zoomFactor = Int(log2(zoomWidth)) - 9
-        print("...REGION DID CHANGE: ZOOM FACTOR \(zoomFactor)")
-        startCoord.accept(mapView.centerCoordinate)
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        if let coordinate =  view.annotation?.coordinate,
+           mapView == mapViewEnd {
+            print(coordinate)
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, didDeselect view: MKAnnotationView) {
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+        views.forEach { view in
+            print(view.annotation?.coordinate)
+        }
+        
+        tableView.reloadData()
     }
 }
 
@@ -116,4 +220,8 @@ extension TodoUpdateTableViewController: CLLocationManagerDelegate {
         centerAnnotation.coordinate = locValue
         mapViewStart.centerCoordinate = locValue
     }
+}
+
+class EndLocationCell: UITableViewCell {
+    
 }
